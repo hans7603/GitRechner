716a9c7448cc731ef561acf341c2cf2fdbabf248
hans7603 <51442734+hans7603@users.noreply.github.com>
2019-06-08 11:11:06 +0200

    Add files via upload

---
+++ action-template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from hermes_python.hermes import Hermes, MqttOptions
+import datetime
+import random
+import toml
+
+
+USERNAME_INTENTS = "hans7603"
+MQTT_BROKER_ADDRESS = "localhost:1883"
+MQTT_USERNAME = None
+MQTT_PASSWORD = None
+
+
+def user_intent(intentname):
+    return USERNAME_INTENTS + ":" + intentname
+
+
+def subscribe_intent_callback(hermes, intent_message):
+    intentname = intent_message.intent.intent_name
+
+    result_sentence = "Hallo, das ist eine Meldung!"
+    current_session_id = intent_message.session_id
+    hermes.publish_end_session(current_session_id, result_sentence)
+
+"""
+    if intentname == user_intent("currentDate"):
+        year = datetime.datetime.now().year
+        month = datetime.datetime.now().month
+        day = datetime.datetime.now().day
+        weekday = datetime.datetime.now().isoweekday()
+        weekday_list = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag']
+        result_sentence = "Heute ist {0}, der {1}.{2}.{3} .".format(weekday_list[weekday - 1], day, month, year)
+        current_session_id = intent_message.session_id
+        hermes.publish_end_session(current_session_id, result_sentence)
+"""
+
+if __name__ == "__main__":
+    snips_config = toml.load('/etc/snips.toml')
+    if 'mqtt' in snips_config['snips-common'].keys():
+        MQTT_BROKER_ADDRESS = snips_config['snips-common']['mqtt']
+    if 'mqtt_username' in snips_config['snips-common'].keys():
+        MQTT_USERNAME = snips_config['snips-common']['mqtt_username']
+    if 'mqtt_password' in snips_config['snips-common'].keys():
+        MQTT_PASSWORD = snips_config['snips-common']['mqtt_password']
+
+    mqtt_opts = MqttOptions(username=MQTT_USERNAME, password=MQTT_PASSWORD, broker_address=MQTT_BROKER_ADDRESS)
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intents(subscribe_intent_callback).start()
