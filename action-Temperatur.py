fc80fd1c06ca28d5d8adcee3e5c5b73b3371fac5
hans7603 <51442734+hans7603@users.noreply.github.com>
2019-06-11 11:30:23 +0200

    Update action-Temperatur.py

--- action-Temperatur.py
+++ action-Temperatur.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+"""
 from hermes_python.hermes import Hermes, MqttOptions
 import datetime
 import random
@@ -35,3 +36,17 @@ if __name__ == "__main__":
     mqtt_opts = MqttOptions(username=MQTT_USERNAME, password=MQTT_PASSWORD, broker_address=MQTT_BROKER_ADDRESS)
     with Hermes(mqtt_options=mqtt_opts) as h:
         h.subscribe_intents(subscribe_intent_callback).start()
+"""
+
+from hermes_python.hermes import Hermes
+
+def action_wrapper(hermes, intent_message):
+    result_sentence = "Heute ist es ziemlich heiß"
+
+    current_session_id = intent_message.session_id
+    hermes.publish_end_session(current_session_id, result_sentence)
+
+
+if __name__ == "__main__":
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intent("kaiserdom:Temperatur", action_wrapper).start()
